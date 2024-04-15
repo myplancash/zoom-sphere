15196fb08960c4ceedee6ca94f5e5a54d14843cb
sergio esteban torres <myplancash@gmail.com>
2024-04-14 20:57:43 -0500

    created a custom hook for Meeting calls setup

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+'use client'
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import React, {useState} from 'react'
+import { useGetCallById } from '../../../../../hooks/useGetCallById';
+import Loader from '@/components/Loader';
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({ params: {id} }: { params: { id: string } }) => {
+  //also have to grab our currently authenticated user
+  const {user, isLoaded} = useUser();
+  const [isSetupCompleted, setIsSetupCompleted] = useState(false)
+  const {call, isCallLoading} = useGetCallById(id)
+
+  if(!isLoaded || isCallLoading) return <Loader/>
+
+
   return (
-    <div>Meeting room number: {params. id}</div>
+    <main className='h-screen w-full'>
+      <StreamCall call={}>
+        <StreamTheme>
+            {!isSetupCompleted ? (
+              <MeetingSetup/>
+            ) : (
+              <MeetingRoom/>
+            )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
   )
 }
 
